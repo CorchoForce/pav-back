b1660c298d6043d5c34a3ef09fab3728b310774f
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-06-05 13:20:16 -0300

    FIxing cpf validator function

--- src/utils/cpf.js
+++ src/utils/cpf.js
@@ -1,22 +1,42 @@
 const validateCPF = (cpf) => {
-  let sum;
-  let remainder;
-  sum = 0;
-  cpf = cpf.replace(".", "").replace('-', "")
-  if (cpf == "00000000000") return false;
+  cpf = cpf.replace(/[^\d]+/g, '');
+  if (cpf == '') return false;
 
-  for (i=1; i<=9; i++) sum = sum + parseInt(cpf.substring(i-1, i)) * (11 - i);
-  remainder = (sum * 10) % 11;
+  if (cpf.length != 11 ||
+    cpf == "00000000000" ||
+    cpf == "11111111111" ||
+    cpf == "22222222222" ||
+    cpf == "33333333333" ||
+    cpf == "44444444444" ||
+    cpf == "55555555555" ||
+    cpf == "66666666666" ||
+    cpf == "77777777777" ||
+    cpf == "88888888888" ||
+    cpf == "99999999999")
+    return false;
 
-  if ((remainder == 10) || (remainder == 11))  remainder = 0;
-  if (remainder != parseInt(cpf.substring(9, 10)) ) return false;
+  add = 0;
+  for (i = 0; i < 9; i++)
+    add += parseInt(cpf.charAt(i)) * (10 - i);
 
-  sum = 0;
-  for (i = 1; i <= 10; i++) sum = sum + parseInt(cpf.substring(i-1, i)) * (12 - i);
-  remainder = (sum * 10) % 11;
+  rev = 11 - (add % 11);
+  if (rev == 10 || rev == 11)
+    rev = 0;
 
-  if ((remainder == 10) || (remainder == 11))  remainder = 0;
-  if (remainder != parseInt(cpf.substring(10, 11) ) ) return false;
+  if (rev != parseInt(cpf.charAt(9)))
+    return false;
+
+  add = 0;
+  for (i = 0; i < 10; i++)
+    add += parseInt(cpf.charAt(i)) * (11 - i);
+
+  rev = 11 - (add % 11);
+  if (rev == 10 || rev == 11)
+    rev = 0;
+
+  if (rev != parseInt(cpf.charAt(10)))
+    return false;
+
   return true;
 }
 
