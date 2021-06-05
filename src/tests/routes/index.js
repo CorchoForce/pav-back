daea72f7363e301aa35ffc93a5264e2318a3eef6
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-06-05 15:48:44 -0300

    Fixing unit test error

--- src/tests/routes/index.js
+++ src/tests/routes/index.js
@@ -59,7 +59,7 @@ describe("Autentication ", () => {
       .end((err, res) => {
         res.should.have.status(422);
         res.body.should.be.a("object");
-        res.body.message.should.be.eql("Email já registrado");
+        res.body.message.should.be.eql("CPF ou Email já registrado");
         done();
       });
   });
