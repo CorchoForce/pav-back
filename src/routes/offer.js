67383e8f4f0cad4e5afa8b56e60449c376fd6bab
rpalmeira <rpalmeira1999@poli.ufrj.br>
2021-06-06 01:41:04 -0300

    * offer route

--- src/routes/offer.js
+++ src/routes/offer.js
@@ -3,7 +3,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
 const authenticate = require("../middlewares/authenticate");
-const { canDelete, canUpdate } = require("../services/permissions/offer");
 
 router.post("/", authenticate, (req, res, next) => {
   const user = req.authUser;
